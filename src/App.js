40126cac769009886c9a0ab32c119c20892be8b1
kshitij-singh-au5 <56304196+kshitij-singh-au5@users.noreply.github.com>
2020-06-10 00:08:48 +0530

    lol

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React,{Component} from 'react';
 import Table from './Component/Table'
 import Order from './Component/Order'
 import Waiter from './Component/Waiter'
-import {BrowserRouter, Route, Link, Redirect} from 'react-router-dom'
+import {BrowserRouter, Route, Link, Redirect, Switch} from 'react-router-dom'
 
 
 class App extends Component {
   render() { 
     return (<BrowserRouter>
+    <Switch>
     <Route path="/" exact><Redirect to="/table"/></Route>
-    <Route path="/table" component={Table} />
-    <Route path="/waiter" component={Waiter} />
-    <Route path="/order" component={Order} />
+    <Route path="/table" component={Table}  exact/>
+    <Route path="/waiter" component={Waiter}  exact/>
+    <Route path="/order" component={Order}  exact/>
+    </Switch>
     </BrowserRouter>  );
   }
 }
